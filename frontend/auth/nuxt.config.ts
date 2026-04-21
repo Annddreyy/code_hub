0a9ef1afd4df2456891aac4d9057a93d2b67cd8e
Andrey22 <andreymail22112006@gmail.com>
2026-04-21 22:53:24 +0300

    feat: Добавить базовую настройку nginx

--- frontend/auth/nuxt.config.ts
+++ frontend/auth/nuxt.config.ts
@@ -1,10 +1,26 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// frontend/auth/nuxt.config.ts
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+
+  app: {
+    baseURL: '/auth/',
+    head: {
+      link: [{ rel: 'icon', type: 'image/svg+xml', href: '/icon.svg' }],
+    },
+  },
+
   vite: {
     optimizeDeps: {
       include: ['@vue/devtools-core', '@vue/devtools-kit'],
+    },
+    server: {
+      allowedHosts: ['codehub.localhost.com'],
+      hmr: {
+        protocol: 'ws',
+        host: 'codehub.localhost.com',
+        port: 80,
+      },
     },
   },
 
